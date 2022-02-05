ab0b7c052d3350919818168ddb07c0bcc5d3410f
Sahil Kumar <99073656+SahilKumar4@users.noreply.github.com>
2022-02-05 17:09:01 +0530

    Table 1-100
    write table 1-100 in 5 sec in C++

---
+++ tables.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+ cout<<"\n\n\n";
+  cout<<"-------------------------------------------\n\n";
+ int a=1,b=1,c;
+
+   while(a<=20)
+   {
+     while(b<=10)
+     {
+       c=a*b;
+       b=b+1;
+       cout<<c<<"  ";
+     }
+
+       a=a+1;
+       b=1;
+        cout<<"\n\n\n";
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+  cout<<"\n\n\n";
+  return 0;
+}
